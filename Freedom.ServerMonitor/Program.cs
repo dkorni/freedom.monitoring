0d357ff0462e595df3fb0eae7641fdfcf267623d
Denys.Korniienko <den.creativ.kor@gmail.com>
2023-01-13 16:33:18 +0200

    Added entity framework

--- Freedom.ServerMonitor/Program.cs
+++ Freedom.ServerMonitor/Program.cs
@@ -1,4 +1,5 @@
 using Freedom.ServerMonitor.Contracts;
+using Freedom.ServerMonitor.DataBase;
 using Freedom.ServerMonitor.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IServerInfoRepository, MemoryRepository>();
+builder.Services.AddDbContext<DataBaseContext>();
+builder.Services.AddScoped<IServerInfoRepository, DbRepository>();
 
 var app = builder.Build();
 
---
+++ Freedom.ServerMonitor/Repositories/DbRepository.cs
@@ -0,0 +1,27 @@
+using System.Data.Entity;
+using Freedom.ServerMonitor.Contracts;
+using Freedom.ServerMonitor.DataBase;
+using Freedom.ServerMonitor.Models;
+
+namespace Freedom.ServerMonitor.Repositories;
+
+public class DbRepository : IServerInfoRepository
+{
+    private readonly DataBaseContext _context;
+
+    public DbRepository(DataBaseContext context)
+    {
+        _context = context;
+    }
+    
+    public Task<ServerInfoModel[]> GetAll()
+    {
+        return Task.Run(() => _context.ServerInfos.ToArray());
+    }
+
+    public Task Create(ServerInfoModel serverInfo)
+    {
+        _context.Add(serverInfo);
+        return _context.SaveChangesAsync();
+    }
+}
